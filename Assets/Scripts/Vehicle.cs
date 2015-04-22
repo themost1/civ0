9e01166f32b9a2ce3cf786c6ba26acb983a80d24
William Ruppenthal <williamruppenthal42@gmail.com>
2015-04-22 09:03:54 -0700

    Dank skybox

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Vehicle : MonoBehaviour {
 	public int player;
 	public bool moved;
-	float health=100;
+	public float health=100;
 	// Use this for initialization
 	void Start () {
 		moved = false;
