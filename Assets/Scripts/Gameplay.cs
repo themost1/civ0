a87920b4df93306963f989e7a51e6cd5c03b41f7
William Ruppenthal <williamruppenthal42@gmail.com>
2015-04-22 12:05:42 -0700

    Code cleaned up

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -6,7 +6,9 @@ public class Gameplay : MonoBehaviour {
 	public GameObject tank;
 	public static GameObject selected;
 	public int vNum;
-	public ArrayList vehicles = new ArrayList();
+	public ArrayList p1vehicles = new ArrayList();
+	public ArrayList p2vehicles = new ArrayList();
+
 	// Use this for initialization
 	void Start () {
 		cooldown = 0.1f;
@@ -23,67 +25,65 @@ public class Gameplay : MonoBehaviour {
 				Vector3 pos = SelectUnit.clickedHex.worldPosition;
 				pos.y = 0.4f;
 				vehicle = (GameObject)Instantiate(tank, pos, new Quaternion(0,0,0,0));
-				currentPlayer=2;
 				SelectUnit.clickedHex.full = true;
 			}
 
 			if(vehicle != null){
-				MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
-				foreach(MeshRenderer mr in chillin)
-					mr.material.color = Color.blue;
-
-				vehicles.Add (vehicle);
+				if(currentPlayer == 1){
+					MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
+					foreach(MeshRenderer mr in chillin)
+						mr.material.color = Color.blue;
+					p1vehicles.Add (vehicle);
+					currentPlayer = 2;
+				}
+				else if(currentPlayer == 2){
+					MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
+					foreach(MeshRenderer mr in chillin)
+						mr.material.color = Color.red;
+					p2vehicles.Add (vehicle);
+					currentPlayer = 1;
+				}
 			}
 			turn++;
-		} 
-		else if (Input.GetKey ("5") && currentPlayer==2) {
-			GameObject vehicle = null;
-
-			if(SelectUnit.clickedHex != null && !SelectUnit.clickedHex.full){
-				Vector3 pos = SelectUnit.clickedHex.worldPosition;
-				pos.y = 0.4f;
-				vehicle = (GameObject)Instantiate(tank, pos, new Quaternion(0,0,0,0));
-				vehicle.transform.Rotate(Vector3.right);
-				currentPlayer=1;
-				SelectUnit.clickedHex.full = true;
-			}
-
-			if(vehicle != null){
-				MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
-				foreach(MeshRenderer mr in chillin)
-					mr.material.color = Color.red;
-				vehicles.Add (vehicle);
-			}
 		}
 
-		//selectVehicle ();
-		foreach (GameObject go in vehicles) {
-			if(go != null && SelectUnit.clickedHex != null)
-				if (Vector3.Distance(go.transform.position, SelectUnit.clickedHex.worldPosition)<=1)
-					selected=go;
-			go.GetComponent<Vehicle>().moved=false;
-		}
-		if(selected != null)
-			if (Vector3.Distance (selected.transform.position, SelectUnit.clickedHex.worldPosition) > 1)
-				selected = null;
+		selectVehicle ();
+//		foreach (GameObject go in vehicles) {
+//			if(go != null && SelectUnit.clickedHex != null)
+//				if (Vector3.Distance(go.transform.position, SelectUnit.clickedHex.worldPosition)<=1)
+//					selected=go;
+//			go.GetComponent<Vehicle>().moved=false;
+//		}
+//		if(selected != null)
+//			if (Vector3.Distance (selected.transform.position, SelectUnit.clickedHex.worldPosition) > 1)
+//				selected = null;
 	}
+
 	void selectVehicle() {
-		if (selected.GetComponent<Vehicle> ().moved && cooldown == 0)
-			cooldown = 0.1f;
-		else if (selected.GetComponent<Vehicle> ().moved && cooldown>0.3) {
-			selected=(GameObject)vehicles[vNum];
-			cooldown=0f;
-			vNum++;
-		}
-		GameObject finalVehicle = (GameObject)vehicles [vehicles.Count - 1];
-		if (vNum == vehicles.Count && finalVehicle.GetComponent<Vehicle>().moved) {
-			GameObject ve = (GameObject)vehicles[vehicles.Count-1];
-			print (ve.GetComponent<Vehicle>().moved);
-			foreach(GameObject v in vehicles)
-				v.GetComponent<Vehicle>().moved=false;
-			vNum=0;
-			selected=null;
-			cooldown=0.1f;
-		}
+//		if (selected.GetComponent<Vehicle> ().moved && cooldown == 0)
+//			cooldown = 0.1f;
+//		else if (selected.GetComponent<Vehicle> ().moved && cooldown>0.3) {
+//			selected=(GameObject)vehicles[vNum];
+//			cooldown=0f;
+//			vNum++;
+//		}
+//		GameObject finalVehicle = (GameObject)vehicles [vehicles.Count - 1];
+//		if (vNum == vehicles.Count && finalVehicle.GetComponent<Vehicle>().moved) {
+//			GameObject ve = (GameObject)vehicles[vehicles.Count-1];
+//			print (ve.GetComponent<Vehicle>().moved);
+//			foreach(GameObject v in vehicles)
+//				v.GetComponent<Vehicle>().moved=false;
+//			vNum=0;
+//			selected=null;
+//			cooldown=0.1f;
+//		}
+		if(currentPlayer == 1)
+			foreach(GameObject go in p1vehicles)
+				if(Vector3.Distance(go.transform.position,SelectUnit.clickedHex.worldPosition) < 1)
+					selected = go;
+		else if(currentPlayer == 2)
+			foreach(GameObject go2 in p2vehicles)
+				if(Vector3.Distance(go.transform.position,SelectUnit.clickedHex.worldPosition) < 1)
+					selected = go2;
 	}
 }
