e3a2cd1ab166077a0e763c983b2665a33ed1a90d
William Ruppenthal <williamruppenthal42@gmail.com>
2015-04-23 16:03:08 -0700

    should have working spawns now

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -2,69 +2,73 @@
 using System.Collections;
 
 public class Gameplay : MonoBehaviour {
-	public static float cooldown=0, currentPlayer=1, turn;
+	public static float cooldown=0;
+	public float spawnRadius;
+	public static int currentPlayer=1;
 	public GameObject tank;
 	public static GameObject selected;
 	public int vNum;
 	public ArrayList vehicles = new ArrayList();
+
 	// Use this for initialization
 	void Start () {
 		cooldown = 0.1f;
+		spawnRadius = 5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (cooldown>0)
 			cooldown += Time.deltaTime;
-		if (Input.GetKey ("6") && currentPlayer==1) {
-			currentPlayer=2;
-			GameObject vehicle;
-			vehicle = (GameObject)Instantiate(tank, new Vector3(0,0.3f,1.5f*turn), new Quaternion(0,0,0,0));
-			MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
-			foreach(MeshRenderer mr in chillin)
-				mr.material.color = Color.blue;
+		if (Input.GetKey ("6")){
+			GameObject vehicle = null;
 
-			vehicles.Add (vehicle);
-			turn++;
-			currentPlayer=2;
-		} else if (Input.GetKey ("5") && currentPlayer==2) {
-			currentPlayer=1;
-			GameObject vehicle;
-			vehicle = (GameObject)Instantiate(tank, new Vector3(1.7f,0.3f,1.5f*turn), new Quaternion(0,0,0,0));
-			MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
-			foreach(MeshRenderer mr in chillin)
-				mr.material.color = Color.red;
+			bool canSpawn = false;
+			int playersTanks = 0; //number of tanks for that player
 
-			vehicles.Add (vehicle);
+			foreach(GameObject go in vehicles)
+				if(go.GetComponent<Vehicle>().player == currentPlayer)
+					playersTanks++;
+
+			if(playersTanks == 0)
+				canSpawn = true;
+			else
+				foreach(GameObject go in vehicles)
+					if(Vector3.Distance(go.transform.position,SelectUnit.clickedHex.worldPosition) < spawnRadius)
+						canSpawn = true;
+			
+			if(canSpawn){
+				if(SelectUnit.clickedHex != null && !SelectUnit.clickedHex.full){
+					Vector3 pos = SelectUnit.clickedHex.worldPosition;
+					pos.y = 0.4f;
+					vehicle = (GameObject)Instantiate(tank, pos, new Quaternion(0,0,0,0));
+					vehicle.GetComponent<Vehicle>().player = currentPlayer;
+					MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
+					foreach(MeshRenderer mr in chillin)
+						if(currentPlayer == 1)	
+							mr.material.color = Color.blue;
+						else if(currentPlayer == 2)
+							mr.material.color = Color.red;
+					
+					if(currentPlayer == 2)
+						vehicle.transform.Rotate(0,180f,0);
+
+					vehicles.Add(vehicle);
+
+					SelectUnit.clickedHex.full = true;
+				}
+			}
 		}
-		//selectVehicle ();
-		foreach (GameObject go in vehicles) {
-			if(go != null && SelectUnit.clickedHex != null)
-				if (Vector3.Distance(go.transform.position, SelectUnit.clickedHex.worldPosition)<=1)
-					selected=go;
-			go.GetComponent<Vehicle>().moved=false;
-		}
-		if(selected != null)
-			if (Vector3.Distance (selected.transform.position, SelectUnit.clickedHex.worldPosition) > 1)
-				selected = null;
+		
+		
+
+		selectVehicle ();
 	}
+
 	void selectVehicle() {
-		if (selected.GetComponent<Vehicle> ().moved && cooldown == 0)
-			cooldown = 0.1f;
-		else if (selected.GetComponent<Vehicle> ().moved && cooldown>0.3) {
-			selected=(GameObject)vehicles[vNum];
-			cooldown=0f;
-			vNum++;
-		}
-		GameObject finalVehicle = (GameObject)vehicles [vehicles.Count - 1];
-		if (vNum == vehicles.Count && finalVehicle.GetComponent<Vehicle>().moved) {
-			GameObject ve = (GameObject)vehicles[vehicles.Count-1];
-			print (ve.GetComponent<Vehicle>().moved);
-			foreach(GameObject v in vehicles)
-				v.GetComponent<Vehicle>().moved=false;
-			vNum=0;
-			selected=null;
-			cooldown=0.1f;
-		}
+		foreach(GameObject go in vehicles)
+			if(go.GetComponent<Vehicle>().player == currentPlayer)
+				if(Vector3.Distance(go.transform.position,SelectUnit.clickedHex.worldPosition) < 1)
+					selected = go;
 	}
 }
