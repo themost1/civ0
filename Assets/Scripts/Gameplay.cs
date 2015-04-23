becd9a8888e84857730d7041b0762200dfb5f990
William Ruppenthal <williamruppenthal42@gmail.com>
2015-04-23 09:40:37 -0700

    Works except player doesn't change

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -2,16 +2,18 @@
 using System.Collections;
 
 public class Gameplay : MonoBehaviour {
-	public static float cooldown=0, currentPlayer=1, turn;
+	public static float cooldown=0;
+	public float spawnRadius;
+	public static int currentPlayer=1;
 	public GameObject tank;
 	public static GameObject selected;
 	public int vNum;
-	public ArrayList p1vehicles = new ArrayList();
-	public ArrayList p2vehicles = new ArrayList();
+	public ArrayList vehicles = new ArrayList();
 
 	// Use this for initialization
 	void Start () {
 		cooldown = 0.1f;
+		spawnRadius = 5f;
 	}
 	
 	// Update is called once per frame
@@ -21,69 +23,52 @@ public class Gameplay : MonoBehaviour {
 		if (Input.GetKey ("6")){
 			GameObject vehicle = null;
 
-			if(SelectUnit.clickedHex != null && !SelectUnit.clickedHex.full){
-				Vector3 pos = SelectUnit.clickedHex.worldPosition;
-				pos.y = 0.4f;
-				vehicle = (GameObject)Instantiate(tank, pos, new Quaternion(0,0,0,0));
-				SelectUnit.clickedHex.full = true;
-			}
+			bool canSpawn = false;
+			int playersTanks = 0; //number of tanks for that player
 
-			if(vehicle != null){
-				if(currentPlayer == 1){
+			foreach(GameObject go in vehicles)
+				if(go.GetComponent<Vehicle>().player == currentPlayer)
+					playersTanks++;
+
+			if(playersTanks == 0)
+				canSpawn = true;
+			else
+				foreach(GameObject go in vehicles)
+					if(Vector3.Distance(go.transform.position,SelectUnit.clickedHex.worldPosition) < spawnRadius)
+						canSpawn = true;
+			
+			if(canSpawn){
+				if(SelectUnit.clickedHex != null && !SelectUnit.clickedHex.full){
+					Vector3 pos = SelectUnit.clickedHex.worldPosition;
+					pos.y = 0.4f;
+					vehicle = (GameObject)Instantiate(tank, pos, new Quaternion(0,0,0,0));
+					vehicle.GetComponent<Vehicle>().player = currentPlayer;
 					MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
 					foreach(MeshRenderer mr in chillin)
-						mr.material.color = Color.blue;
-					p1vehicles.Add (vehicle);
-					currentPlayer = 2;
-				}
-				else if(currentPlayer == 2){
-					MeshRenderer[] chillin = vehicle.GetComponentsInChildren<MeshRenderer>();
-					foreach(MeshRenderer mr in chillin)
-						mr.material.color = Color.red;
-					p2vehicles.Add (vehicle);
-					currentPlayer = 1;
+						if(currentPlayer == 1)	
+							mr.material.color = Color.blue;
+						else if(currentPlayer == 2)
+							mr.material.color = Color.red;
+					
+					if(currentPlayer == 2)
+						vehicle.transform.Rotate(0,180f,0);
+
+					vehicles.Add(vehicle);
+
+					SelectUnit.clickedHex.full = true;
 				}
 			}
-			turn++;
 		}
+		
+		
 
 		selectVehicle ();
-//		foreach (GameObject go in vehicles) {
-//			if(go != null && SelectUnit.clickedHex != null)
-//				if (Vector3.Distance(go.transform.position, SelectUnit.clickedHex.worldPosition)<=1)
-//					selected=go;
-//			go.GetComponent<Vehicle>().moved=false;
-//		}
-//		if(selected != null)
-//			if (Vector3.Distance (selected.transform.position, SelectUnit.clickedHex.worldPosition) > 1)
-//				selected = null;
 	}
 
 	void selectVehicle() {
-//		if (selected.GetComponent<Vehicle> ().moved && cooldown == 0)
-//			cooldown = 0.1f;
-//		else if (selected.GetComponent<Vehicle> ().moved && cooldown>0.3) {
-//			selected=(GameObject)vehicles[vNum];
-//			cooldown=0f;
-//			vNum++;
-//		}
-//		GameObject finalVehicle = (GameObject)vehicles [vehicles.Count - 1];
-//		if (vNum == vehicles.Count && finalVehicle.GetComponent<Vehicle>().moved) {
-//			GameObject ve = (GameObject)vehicles[vehicles.Count-1];
-//			print (ve.GetComponent<Vehicle>().moved);
-//			foreach(GameObject v in vehicles)
-//				v.GetComponent<Vehicle>().moved=false;
-//			vNum=0;
-//			selected=null;
-//			cooldown=0.1f;
-//		}
-		if(currentPlayer == 1)
-			foreach(GameObject go in p1vehicles)
+		foreach(GameObject go in vehicles)
+			if(go.GetComponent<Vehicle>().player == currentPlayer)
 				if(Vector3.Distance(go.transform.position,SelectUnit.clickedHex.worldPosition) < 1)
 					selected = go;
-		else if(currentPlayer == 2)
-			foreach(GameObject go2 in p2vehicles)
-				if(Vector3.Distance(go.transform.position,SelectUnit.clickedHex.worldPosition) < 1)
-					selected = go2;
 	}
 }
