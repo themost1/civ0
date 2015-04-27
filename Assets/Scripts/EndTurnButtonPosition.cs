3de872d1a572868eb3edf4795fecd2d47d4e94b8
William Ruppenthal <williamruppenthal42@gmail.com>
2015-04-27 09:44:20 -0700

    Added end turn button but it doesn't do anything

---
+++ Assets/Scripts/EndTurnButtonPosition.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTurnButtonPosition : MonoBehaviour {
+	void Update () {
+		transform.position = new Vector3(Screen.width - 90f,25f);
+	}
+}
