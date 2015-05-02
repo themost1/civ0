5a7e3a4c8f2838437c2c142032bf9a40057df4d8
William Ruppenthal <williamruppenthal42@gmail.com>
2015-05-02 15:00:46 -0700

    End turn button works

---
+++ Assets/Scripts/EndTurnButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTurnButtonScript : MonoBehaviour {
+
+	public void endTurn(){
+		if(Gameplay.currentPlayer == 1)
+			Gameplay.currentPlayer = 2;
+		else
+			Gameplay.currentPlayer = 1;
+	}
+}
